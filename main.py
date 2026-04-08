a70df8684198d1a52838cadac20185720023f14e
MorokStary <gardungadun@gmail.com>
2026-04-08 13:07:48 +0300

    Movement

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ pygame.display.set_caption("Octopus")
 
 clock = pygame.time.Clock()# < -----------
 
-#_______PLAYER_SPRITE______
 
+#_______PLAYER_SPRITE______
+player_speed = 5
 
 player_surface = pygame.image.load("Asset/player.png") # .convert_alpha()
 player_rect = player_surface.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//2))
@@ -27,6 +28,25 @@ player_rect = player_surface.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//2))
 
 
 while True:
+    
+    
+    
+    # ___________________________________________________
+    keys = pygame.key.get_pressed()
+    
+    direction = pygame.math.Vector2(0, 0)
+    direction.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+    direction.y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
+
+    if direction.length() > 0:
+        direction = direction.normalize()
+
+    player_rect+= direction + player_speed
+
+    # ___________________________________________________
+
+
+
     # event - це є наша локальна змінна 
     for event in pygame.event.get():
 
@@ -38,11 +58,6 @@ while True:
         pass
         
 
-        if event.type == pygame.A:
-            player_rect
-
-
-            pass
     
     screen.fill((30,30,50))
 
